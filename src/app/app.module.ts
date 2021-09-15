cc8f21c1e709606c9f862048ed9bd36350445b13
PulockDas <pulock46@student.sust.edu>
2021-09-16 01:10:41 +0600

    Authentication Page initiated. AuthService created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,16 @@ import { PerStudentComponent } from './Student/all-students/perStudent/per-stude
 import { AuthComponent } from './Authentication/auth/auth.component';
 import { AddCGPAComponent } from './addCGPA/add-cgpa/add-cgpa.component';
 
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +28,16 @@ import { AddCGPAComponent } from './addCGPA/add-cgpa/add-cgpa.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
